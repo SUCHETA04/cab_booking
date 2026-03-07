91aaeb926af227c411d1467e8a15748b18684be9
SUCHETA04 <biswassucheta04@gmail.com>
2026-03-07 10:29:17 +0530

    final

--- frontend/src/components/RideMap.jsx
+++ frontend/src/components/RideMap.jsx
@@ -17,22 +17,20 @@ L.Icon.Default.mergeOptions({
     shadowUrl: shadowUrl,
 });
 
-const greenIcon = new L.Icon({
-    iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-green.png',
-    shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
+const greenIcon = new L.DivIcon({
+    html: `<img src="https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-green.png" style="width: 25px; height: 41px;" />`,
+    className: 'custom-div-icon',
     iconSize: [25, 41],
     iconAnchor: [12, 41],
-    popupAnchor: [1, -34],
-    shadowSize: [41, 41]
+    popupAnchor: [1, -34]
 });
 
-const redIcon = new L.Icon({
-    iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-red.png',
-    shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
+const redIcon = new L.DivIcon({
+    html: `<img src="https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-red.png" style="width: 25px; height: 41px;" />`,
+    className: 'custom-div-icon',
     iconSize: [25, 41],
     iconAnchor: [12, 41],
-    popupAnchor: [1, -34],
-    shadowSize: [41, 41]
+    popupAnchor: [1, -34]
 });
 
 const ChangeView = ({ center, zoom, bounds }) => {
@@ -66,7 +64,14 @@ const RoutingControl = ({ pickupLat, pickupLng, dropoffLat, dropoffLng }) => {
             lineOptions: {
                 styles: [{ color: '#3b82f6', weight: 4 }]
             },
-            createMarker: function () { return null; } // Re-use the React-Leaflet markers instead
+            createMarker: function (i, waypoint, n) {
+                const markerIcon = (i === 0) ? greenIcon : redIcon;
+                const marker = L.marker(waypoint.latLng, {
+                    draggable: false,
+                    icon: markerIcon
+                });
+                return marker;
+            }
         }).addTo(map);
 
         // Completely hide the generic routing control container from the UI
